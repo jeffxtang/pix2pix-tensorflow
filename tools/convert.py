c0b434db9ae4d75ab6443546392a82bde4de0567
jeffxtang <jeff.x.tang@gmail.com>
2018-03-05 22:45:45 -0800

    removed code not needed

--- tools/convert.py
+++ tools/convert.py
@@ -5,9 +5,3 @@ for f in file_names:
     if f.find(".png") != -1:
     	os.system("convert photos/resized/" + f + " -blur 0x3 photos/blurry/" + f)
     	print("photos/resized/" + f)
-
-file_names = os.listdir("photos/resized/val/")
-for f in file_names:
-    if f.find(".png") != -1:
-        os.system("convert photos/resized/" + f + " -blur 0x3 photos/blurry/" + f)
-        print("photos/resized/" + f)
